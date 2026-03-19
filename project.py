ed89b018cad0f8742c2439c63e9da68fe9c105e6
HRB05 <hrb10@kent.ac.uk>
2026-03-19 19:39:13 +0000

    Idea - simple idea for project

---
+++ project.py
@@ -0,0 +1,7 @@
+# Card collection game
+
+
+#1) Make a class for the different packs : Price | Luck %
+
+#2) Make a class for base cards : Value | Rarity | Condition
+# Child classes will be cards with mutations and higher rarity
