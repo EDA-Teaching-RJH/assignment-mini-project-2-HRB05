2ef818ccd54a080126a7074bcb33a137729712fc
HRB05 <hrb10@kent.ac.uk>
2026-03-25 18:57:21 +0000

    Made a way to see your card you just got

--- project.py
+++ project.py
@@ -9,6 +9,7 @@ class Packs:
         self.price = price
         self.luck = luck
 
+    global mutations
     mutations = ["Common","Gold","Diamond","Unreal","God"]
     global weights
     weights = [68.99,25,5,1,0.01] # Weights to get each mutation with a pack luck of 1
@@ -17,7 +18,6 @@ class Packs:
     result = random.choices(mutations, weights=weights, k=1)[0]
     global idx
     idx = mutations.index(result)
-    print(result,idx)
 
 #2) Make a class for base cards : Value | Rarity | Condition
 
@@ -25,29 +25,43 @@ class Card:
     def __init__(self,value,income,rarity,condition):
         self.value = value
         self.income = income
-        self.rarityVal = rarity
+        self.rarity = rarity
         self.condition = condition
 
 # Child classes will be cards with mutations and higher rarity
 
 class Mutation(Card):
-    def __init__(self,value,income,rarity,condition,mutation,rarityx):
+    def __init__(self,value,income,rarity,condition,mutation):
         super().__init__(value, income, rarity, condition)
         self.mutation = mutation
-        self.rarityx = rarityx
-        self.rarity = float(self.rarityVal*self.rarityx)
+
 
 cards = []
 con = random.randint(1,100)
-print(con)
 rarityDenom = round((100/weights[idx])*(con/100),1)
+val = rarityDenom*10
+inc = rarityDenom
 if (rarityDenom) <= 1:
      rarityDenom = 1
 rarityString = f"1/{rarityDenom}"
-print(rarityString)
 if result == "Common":
         newCard = Card(
-            value = (10*(con/50)), # Means that the value is only increased if condition >= 50
-            income = 1,
-            rarity = rarityString
+            value = val,
+            income = inc,
+            rarity = rarityString,
+            condition = con
         )
+        mut = False
+else:
+    newCard = Mutation(
+          value = val,
+          income = rarityDenom,
+          rarity = rarityString,
+          condition = con,
+          mutation = mutations[idx]
+     )
+    mut = True
+
+print(f"Rarity | {newCard.rarity}\nCondition | {newCard.condition}\nValue | {newCard.value}\nIncome | {newCard.income}")
+if mut == True:
+     print(f"Mutation | {newCard.mutation}")
